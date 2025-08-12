7bc51691a38e4b9988f18e5098abbf945f05a999
Varun Sharma <galvevarun@gmail.com>
2025-08-12 22:59:43 +0530

    Initial Commit

---
+++ app/Landingpage/page.js
@@ -0,0 +1,19 @@
+const Landingpage = () => {
+    return (
+        <nav>
+            <div className="max-w-7xl mx-auto px-0 flex items-center justify-between h-18">
+                <div className="text-xl font-bold">
+                    My-App
+                </div>
+                <div className="space-x-6">
+                    <a href="#" className="hover:text-blue-500">Order Now</a>
+                    <a href="#" className="hover:text-blue-500">Contact Us</a>
+                    <a href="#" className="hover:text-blue-500">Menu</a>
+                    
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default Landingpage;
